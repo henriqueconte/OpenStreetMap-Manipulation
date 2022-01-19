3e183946cfd942d6a9602fedd071448d6b301a09
henriqueconte <henriqueconte6@gmail.com>
2022-01-19 14:38:19 +0100

    Implemented question 11 hardcoded

--- server/q11.py
+++ server/q11.py
@@ -3,17 +3,32 @@ from sys import argv
 import re
 import drawer
 
+########
+#
+# How to execute: python3 q11.py 5.7 5.8 45.1 45.2 1000 1000
+#
+########
 
-def parse_linestring(linestring):
+# We are going to receive coordinates, but we need to present them proportionally to the output image size
+def normalize(position, init, end, total_size):
+    proportion = (end - position) / (end - init)
+    return proportion * total_size
+
+# We receive the linestring with all points separated by ",", so we need to cast it into an array of tuples of floats (coordinates).
+def parse_linestring(linestring, init_x, end_x, init_y, end_y, width, height):
     linestring = linestring.replace("LINESTRING(", "")
     linestring = linestring.replace(")", "")
     point_list_string = linestring.split(",")
     point_list = []
 
+    # For every point, we cast it into float and calculate the position that will be displayed on the final image
     for element in point_list_string:
         coordinate = element.split(" ")
-        point_list.append((float(coordinate[0]), float(coordinate[1])))
-    
+
+        point = (normalize(float(coordinate[0]), init_x, end_x, width),
+                normalize(float(coordinate[1]), init_y, end_y, height))
+        point_list.append(point)
+
     return point_list
 
 def execute_query():
@@ -26,8 +41,8 @@ def execute_query():
         width = int(argv[5])
         height = int(argv[6])
 
+        # Instantiating the drawer helper
         image_drawer = drawer.Image(width, height)
-        image_drawer.draw_rectangle(init_x, init_y, end_x, end_y, (100/255, 200/255, 20/255, 255/255), (200/255, 10/255, 60/255, 255/255))
         
         cursor = db.execute_query("""SELECT ST_AsText(linestring)
                                     FROM ways 
@@ -44,10 +59,10 @@ def execute_query():
                                             )
                                         );"""
                 )
-       
+        
+        # For every linestring received, we parse the coordinates and draw them on the final image. 
         for row in cursor:
-            point_list = parse_linestring(row[0])
-            print(point_list)
+            point_list = parse_linestring(row[0], init_x, end_x, init_y, end_y, width, height)
             image_drawer.draw_linestring(point_list, (2/255, 130/255, 200/255, 255/255))
         
         image_drawer.save("map.png")
